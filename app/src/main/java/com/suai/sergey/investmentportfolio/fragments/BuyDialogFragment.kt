e21c6c119cf08108eaec7c84e125d33d4ea5cb5e
pinevpple <alexey.afanasyev95@gmail.com>
2019-06-21 21:21:23 +0300

    fragments classes

--- app/src/main/java/com/suai/sergey/investmentportfolio/fragments/BuyDialogFragment.kt
+++ app/src/main/java/com/suai/sergey/investmentportfolio/fragments/BuyDialogFragment.kt
@@ -1,4 +1,28 @@
 package com.suai.sergey.investmentportfolio.fragments
 
-class BuyDialogFragment {
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.suai.sergey.investmentportfolio.R
+import kotlinx.android.synthetic.main.buy_dialog_fragment_layout.view.*
+
+class BuyDialogFragment : DialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.buy_dialog_fragment_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.button.setOnClickListener {
+            if (!view.amount.text.isBlank() && !view.price.text.isBlank()) {
+                // ЗАПИСЫВАЕМ ПОКУПКУ В БАЗУ
+            } else {
+                // ВЫВЕСТИ ОШИБКУ (КАК ЛУЧШЕ РЕАЛИЗОВАТЬ?)
+            }
+        }
+    }
 }
--- app/src/main/java/com/suai/sergey/investmentportfolio/fragments/SellDialogFragment.kt
+++ app/src/main/java/com/suai/sergey/investmentportfolio/fragments/SellDialogFragment.kt
@@ -1,4 +1,29 @@
 package com.suai.sergey.investmentportfolio.fragments
 
-class SellDialogFragment {
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.suai.sergey.investmentportfolio.R
+import kotlinx.android.synthetic.main.sell_dialog_fragment_layout.view.*
+
+class SellDialogFragment : DialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.sell_dialog_fragment_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.button.setOnClickListener {
+            if (!view.amount.text.isBlank() && !view.price.text.isBlank()) {
+                // ЗАПИСЫВАЕМ ПОКУПКУ В БАЗУ
+            } else {
+                // ВЫВЕСТИ ОШИБКУ
+            }
+        }
+    }
 }
