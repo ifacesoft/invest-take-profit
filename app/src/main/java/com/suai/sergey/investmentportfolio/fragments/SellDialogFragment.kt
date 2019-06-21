e6fb9ad1f65edc988fba5ac80e2a8749410ff9f1
pinevpple <alexey.afanasyev95@gmail.com>
2019-06-21 22:26:59 +0300

    androidx

--- app/src/main/java/com/suai/sergey/investmentportfolio/fragments/SellDialogFragment.kt
+++ app/src/main/java/com/suai/sergey/investmentportfolio/fragments/SellDialogFragment.kt
@@ -1,15 +1,14 @@
 package com.suai.sergey.investmentportfolio.fragments
 
 import android.os.Bundle
-import android.support.v4.app.DialogFragment
+import androidx.fragment.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.suai.sergey.investmentportfolio.R
 import kotlinx.android.synthetic.main.sell_dialog_fragment_layout.view.*
 
-class SellDialogFragment : DialogFragment() {
+class SellDialogFragment : androidx.fragment.app.DialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.sell_dialog_fragment_layout, container, false)
