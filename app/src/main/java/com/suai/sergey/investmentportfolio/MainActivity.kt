10f4619357e67858a8eecab2efa098c3c3a918cd
sergey <sergey021198@yandex.ru>
2019-06-22 00:01:30 +0300

    make spinner2

--- app/src/main/java/com/suai/sergey/investmentportfolio/MainActivity.kt
+++ app/src/main/java/com/suai/sergey/investmentportfolio/MainActivity.kt
@@ -4,20 +4,15 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    private var objects = arrayOf("ГАхзпромсясцо", "ГАхзпромсясцо", "ГАхзпромсясцо")
     lateinit var shortsName: String
     lateinit var longsName: String
     lateinit var costs: String
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,32 +24,53 @@ class MainActivity : AppCompatActivity() {
 
     private fun dataList(): ArrayList<DataClass> {
         val list = ArrayList<DataClass>()
-        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
-        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
-        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
-        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
-        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
-        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
+//        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
+//        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
+//        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
+//        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
+//        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
+//        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
 
         return list
     }
 
+    private fun spinnerList(): ArrayList<String> {
+        val arrayList = ArrayList<String>()
+        arrayList.add(" ")
+        arrayList.add("ГАхзпромсясцо")
+        arrayList.add("ГАхзпромсясцо")
+        arrayList.add("ГАхзпромсясцо")
+        arrayList.add("ГАхзпромсясцо")
+        arrayList.add("ГАхзпромсясцо")
+        arrayList.add("ГАхзпромсясцо")
+        return arrayList
+    }
+
     private fun makeRecycleView() {
-        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        val adapter = DataClassAdapter(dataList())
-        recyclerView.adapter = adapter
+        val tvRecyclerView: TextView = findViewById(R.id.tv_recyclerView)
+
+        if (dataList().size > 0) {
+            tvRecyclerView.visibility = View.INVISIBLE
+            val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+            recyclerView.layoutManager = LinearLayoutManager(this)
+            val adapter = DataClassAdapter(dataList())
+            recyclerView.adapter = adapter
+        } else {
+            tvRecyclerView.visibility = View.VISIBLE
+        }
     }
 
     private fun makeSpinner() {
         val spinner: Spinner = findViewById(R.id.spinner_id)
-        val adapter = ArrayAdapter (this, android.R.layout.simple_spinner_item, objects)
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinner.adapter = adapter
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerList().toArray())
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                val item = parent.getItemAtPosition(position) as String
-                toast(item)
+                if (position != 0) {
+                    val item = parent.getItemAtPosition(position) as String
+                    toast(item)
+                }
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {
