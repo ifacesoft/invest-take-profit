197ae645eff4dcbfd66f62827da3a5819f12c4ad
dp <denis.a.shestakov@gmail.com>
2019-06-21 22:16:30 +0300

    migrate androidx

--- app/src/main/java/com/suai/sergey/investmentportfolio/MainActivity.kt
+++ app/src/main/java/com/suai/sergey/investmentportfolio/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.suai.sergey.investmentportfolio
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.suai.sergey.investmentportfolio.contract.MainContract
 
