0f4f23b14e0019a0898b684c12c5deb2229281af
sergey <sergey021198@yandex.ru>
2019-06-21 22:05:44 +0300

    make recyclerviewx

--- app/src/main/java/com/suai/sergey/investmentportfolio/MainActivity.kt
+++ app/src/main/java/com/suai/sergey/investmentportfolio/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.suai.sergey.investmentportfolio
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+//    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val list = ArrayList<DataClass>()
+        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
+        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
+        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
+        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
+        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
+        list.add(DataClass("USDD", "ГАхзпромсясцо", "10000$$"))
+
+
+        val adapter = DataClassAdapter(list)
+        recyclerView.adapter = adapter
     }
 }
