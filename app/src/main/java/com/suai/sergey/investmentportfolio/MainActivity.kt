e0410736a8ef95ab8ca169b655ee308ea59eddb5
dp <denis.a.shestakov@gmail.com>
2019-06-22 02:12:36 +0300

    background service

--- app/src/main/java/com/suai/sergey/investmentportfolio/MainActivity.kt
+++ app/src/main/java/com/suai/sergey/investmentportfolio/MainActivity.kt
@@ -7,21 +7,33 @@ import com.suai.sergey.investmentportfolio.contract.MainContract
 import com.suai.sergey.investmentportfolio.entity.Stock
 import com.suai.sergey.investmentportfolio.interactors.StockInteractor
 import com.suai.sergey.investmentportfolio.presenters.MainPresenter
+import android.content.Intent
+import com.suai.sergey.investmentportfolio.services.UpdateCurrentPrices
+import android.os.Build
+
+
+
 
 class MainActivity : AppCompatActivity(), MainContract.View {
 
     private var mainPresenter: MainContract.Presenter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        mainPresenter = MainPresenter(this, StockInteractor());
+        mainPresenter = MainPresenter(this, StockInteractor())
         (mainPresenter as MainPresenter).loadStocks()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, UpdateCurrentPrices::class.java)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
+        }
     }
 
     override fun updateStockSpinner(spinners: List<Stock>) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
-
 }
