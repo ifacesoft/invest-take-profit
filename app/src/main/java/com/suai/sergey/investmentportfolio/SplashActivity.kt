2afa9575bfc24bdb51372583a8c51e532d5d9726
pinevpple <alexey.afanasyev95@gmail.com>
2019-06-22 05:37:42 +0300

    anims

--- app/src/main/java/com/suai/sergey/investmentportfolio/SplashActivity.kt
+++ app/src/main/java/com/suai/sergey/investmentportfolio/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.suai.sergey.investmentportfolio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.suai.sergey.investmentportfolio.fragments.SellDialogFragment
@@ -14,5 +15,9 @@ class SplashActivity : AppCompatActivity() {
         imageView.setOnClickListener {
             SellDialogFragment().show(supportFragmentManager, "a")
         }
+
+/*        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout)*/
     }
 }
