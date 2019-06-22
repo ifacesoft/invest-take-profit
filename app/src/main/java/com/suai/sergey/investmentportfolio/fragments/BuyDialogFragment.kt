d59464b636f375c862e5019234e72daeedf6330c
pinevpple <alexey.afanasyev95@gmail.com>
2019-06-22 04:29:43 +0300

    with splash

--- app/src/main/java/com/suai/sergey/investmentportfolio/fragments/BuyDialogFragment.kt
+++ app/src/main/java/com/suai/sergey/investmentportfolio/fragments/BuyDialogFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.suai.sergey.investmentportfolio.R
 import kotlinx.android.synthetic.main.buy_dialog_fragment_layout.view.*
 
 class BuyDialogFragment : DialogFragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_TITLE, android.R.style.Theme_Material_Light_Dialog_Alert)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.buy_dialog_fragment_layout, container, false)
@@ -18,10 +23,11 @@ class BuyDialogFragment : DialogFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.button.setOnClickListener {
-            if (!view.amount.text.isBlank() && !view.price.text.isBlank()) {
+            if (!view.amount.text?.isBlank()!! && !view.price.text?.isBlank()!!) {
                 // ЗАПИСЫВАЕМ ПОКУПКУ В БАЗУ
             } else {
-                // ВЫВЕСТИ ОШИБКУ (КАК ЛУЧШЕ РЕАЛИЗОВАТЬ?)
+                // ВЫВЕСТИ ОШИБКУ
+                Toast.makeText(activity, "Заполни все поля!", Toast.LENGTH_SHORT).show()
             }
         }
     }
--- app/src/main/java/com/suai/sergey/investmentportfolio/fragments/SellDialogFragment.kt
+++ app/src/main/java/com/suai/sergey/investmentportfolio/fragments/SellDialogFragment.kt
@@ -5,10 +5,17 @@ import androidx.fragment.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.suai.sergey.investmentportfolio.R
 import kotlinx.android.synthetic.main.sell_dialog_fragment_layout.view.*
 
-class SellDialogFragment : androidx.fragment.app.DialogFragment() {
+class SellDialogFragment : DialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_TITLE, android.R.style.Theme_Material_Light_Dialog_Alert)
+
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.sell_dialog_fragment_layout, container, false)
@@ -18,10 +25,14 @@ class SellDialogFragment : androidx.fragment.app.DialogFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.button.setOnClickListener {
-            if (!view.amount.text.isBlank() && !view.price.text.isBlank()) {
+            if (!view.amount.text?.isBlank()!! && !view.price.text?.isBlank()!!) {
                 // ЗАПИСЫВАЕМ ПОКУПКУ В БАЗУ
+                val amount: Int = view.amount.text.toString().toInt()
+                val price: Int = view.price.text.toString().toInt()
+                onDestroyView()
             } else {
                 // ВЫВЕСТИ ОШИБКУ
+                Toast.makeText(activity, "Заполни все поля!", Toast.LENGTH_SHORT).show()
             }
         }
     }
